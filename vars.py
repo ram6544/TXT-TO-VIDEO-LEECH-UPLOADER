c68b01ff7fde8f3522334ff904e8480141f7384f
ram6544 <pujanr724@gmail.com>
2025-12-31 19:23:59 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,22 +2,24 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "28389286"))
+API_HASH = environ.get("API_HASH", "b88da5f4f338cca30f8ea5fb53cb083b")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8382290189:AAFN8TDWswjEq1ccvAyvHcpHhOevaCZ_bik")
 
 # Force Subscribe Configuration
 FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "bot_subscription")  # Channel username without @, 
 FORCE_SUB_CHANNEL_LINK = environ.get("FORCE_SUB_CHANNEL_LINK", "https://t.me/bot_subscription")  # Channel link
 
 # Admin Configuration
-ADMINS = list(map(int, environ.get("ADMINS", "").split()))
+ADMINS = list(map(int, environ.get("ADMINS", "6334323103").split()))
 
 # Optional: Bot Owner ID
-OWNER_ID = int(environ.get("OWNER_ID", ""))
+OWNER_ID = int(environ.get("OWNER_ID", "6334323103"))
 
 # Database URL (if you want to add database support later)
-DATABASE_URL = environ.get("DATABASE_URL", "")
+DATABASE_URL = environ.get("DATABASE_URL", "mongodb+srv://divyanshshukla5375_db_user:1kZ2dsVTktdMljpr@cluster0.lo5qk5v.mongodb.net/?appName=Cluster0
+")
+
 
 
 
